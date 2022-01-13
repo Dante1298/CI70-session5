3070561a0f74abae6f0b04e8932e6d2fafa9b92c
Dante <nguyenchinhan1298@gmail.com>
2022-01-13 20:30:42 +0700

    â

---
+++ js/index.js
@@ -0,0 +1,24 @@
+import LoginScreen from "./containers/Login/index.js";
+import RegisterScreen from "./containers/Register/index.js";
+import CheckEmailScreen from "./containers/CheckEmail/index.js";
+
+class App {
+    $activeScreen;
+    constructor()  {}
+    changeActiveScreen(screen) {
+        const appEle = document.getElementById("app");
+        if(appEle) {
+            if (this.$activeScreen) {
+                appEle.innerHTML = "";
+            }
+            this.$activeScreen = screen;
+            appEle.appendChild(screen.render());
+        }
+    }
+}
+
+const app = new App();
+const signUp = new RegisterScreen();
+const checkEmailScreen = new CheckEmailScreen();
+app.changeActiveScreen(signUp);
+export default app;
